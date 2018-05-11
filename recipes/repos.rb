20c2655418864f4f9191cf3c391345782b9c802d
Julien Levasseur <levasju1@LAP-MTL-LEVASJU.vasco.com>
2018-05-11 13:47:18 -0400

    refactor repos mgmt

--- recipes/repos.rb
+++ recipes/repos.rb
@@ -8,22 +8,42 @@ apt_update 'update' do
   action :update
 end
 
-execute 'add_docker_repo_key' do
-  command 'curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -'
-  only_if { node['platform'] == 'debian' }
+platforms = [
+  'debian',
+  'ubuntu'
+]
+
+platforms.each do |platform|
+  execute 'add_docker_repo_key' do
+    command "curl -fsSL https://download.docker.com/linux/#{platform}/gpg | apt-key add -"
+    not_if "apt-key list|grep Docker"
+    only_if { node['platform'] == platform }
+  end
+
+  execute 'add_docker_repo' do
+    command "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/#{platform} $(lsb_release -cs) stable\""
+    only_if { node['platform'] == platform }
+  end
 end
 
-execute 'add_docker_repo_key' do
-  command 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -'
-  only_if { node['platform'] == 'ubuntu' }
-end
+#execute 'add_docker_repo_key' do
+#  command 'curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -'
+#  not_if "apt-key list|grep Docker"
+#  only_if { node['platform'] == 'debian' }
+#end
 
-execute 'add_docker_repo' do
-  command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'
-  only_if { node['platform'] == 'debian' }
-end
+#execute 'add_docker_repo_key' do
+#  command 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -'
+#  not_if "apt-key list|grep Docker"
+#  only_if { node['platform'] == 'ubuntu' }
+#end
 
-execute 'add_docker_repo' do
-  command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
-  only_if { node['platform'] == 'ubuntu' }
-end
+#execute 'add_docker_repo' do
+#  command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'
+#  only_if { node['platform'] == 'debian' }
+#end
+
+#execute 'add_docker_repo' do
+#  command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+#  only_if { node['platform'] == 'ubuntu' }
+#end
